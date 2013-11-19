77bf9b7240c723ec201275cab7bcb5b075b06f52
imGiantChan <530000693@qq.com>
2013-11-19 10:36:07 +0800

    addAct CHANGE

---
+++ unkonw/res/js/activity.js
@@ -0,0 +1,10 @@
+// JavaScript Document
+//图片透明蒙版
+ function openVessel(strId)
+    {
+        document.getElementById(strId).style.display = "block";
+    }
+ function closeVessel(strId)
+    {
+        document.getElementById(strId).style.display = "none";
+    }
